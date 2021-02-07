82ff9e4843e879dce71b53692e21514a417a7dce
Cass Bredeson <c.s.bredeson@gmail.com>
2021-02-07 19:50:19 +0000

    added listings schema and seed listings data

---
+++ db/schema/02_listings.sql
@@ -0,0 +1,17 @@
+-- Drop and recreate Listings table
+
+DROP TABLE IF EXISTS listings CASCADE;
+CREATE TABLE listings (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  description TEXT,
+  price INTEGER NOT NULL,
+  photo_url VARCHAR(255) NOT NULL,
+  creation_date TIMESTAMP,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  featured BOOLEAN DEFAULT FALSE,
+  weight INTEGER NOT NULL,
+  city VARCHAR(255) NOT NULL,
+  sold_date TIMESTAMP DEFAULT NULL,
+  deleted BOOLEAN DEFAULT FALSE
+);
