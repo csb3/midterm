770ba96a0842982531df0e53e9dedd6e06a343f4
Isaac King <idrkings@gmail.com>
2021-02-07 00:10:33 +0000

    Finish styling for listing elements on index

--- testingData.js
+++ testingData.js
@@ -36,7 +36,7 @@ const breads = [
   bread3 = {
     id: 3,
     name: 'Regular Bread',
-    description: 'Full of wheat',
+    description: 'Full of wheat. Giving this a long description so it tries to overflow the box I put it in. No bread, bad bread, stay in your box',
     price: 500,
     photo_url: 'https://i.imgur.com/fPwHJxe.png',
     creation_date: '2020-02-06',
@@ -44,6 +44,96 @@ const breads = [
     featured: false,
     weight: '400g',
     city: 'Vancouver',
+    sold: false,
+    deleted: false
+  },
+
+  bread4 = {
+    id: 4,
+    name: 'Special Reserve',
+    description: 'Good good bread from my special basement bakery',
+    price: 1200,
+    photo_url: 'https://i.imgur.com/VoD6osL.jpg',
+    creation_date: '2020-02-06',
+    user_id: 3,
+    featured: false,
+    weight: '400g',
+    city: 'Vancouver',
+    sold: false,
+    deleted: false
+  },
+
+  bread5 = {
+    id: 5,
+    name: 'Cinnamon Cinnamon',
+    description: 'A welcome respite',
+    price: 800,
+    photo_url: 'https://i.imgur.com/nhoaRcZ.jpg',
+    creation_date: '2020-02-06',
+    user_id: 3,
+    featured: false,
+    weight: '400g',
+    city: 'Winnipeg',
+    sold: false,
+    deleted: false
+  },
+
+  bread6 = {
+    id: 6,
+    name: 'Bread',
+    description: 'Bread',
+    price: 200000,
+    photo_url: 'https://i.imgur.com/nhoaRcZ.jpg',
+    creation_date: '2020-02-06',
+    user_id: 3,
+    featured: false,
+    weight: '400g',
+    city: 'Bread',
+    sold: false,
+    deleted: false
+  },
+
+  bread7 = {
+    id: 6,
+    name: 'Bread',
+    description: 'Bread',
+    price: 200000,
+    photo_url: 'https://i.imgur.com/nhoaRcZ.jpg',
+    creation_date: '2020-02-06',
+    user_id: 3,
+    featured: false,
+    weight: '400g',
+    city: 'Bread',
+    sold: false,
+    deleted: false
+  },
+
+  bread8 = {
+    id: 6,
+    name: 'Bread',
+    description: 'Bread',
+    price: 200000,
+    photo_url: 'https://i.imgur.com/nhoaRcZ.jpg',
+    creation_date: '2020-02-06',
+    user_id: 3,
+    featured: false,
+    weight: '400g',
+    city: 'Bread',
+    sold: false,
+    deleted: false
+  },
+
+  bread9 = {
+    id: 6,
+    name: 'Bread',
+    description: 'Bread',
+    price: 200000,
+    photo_url: 'https://i.imgur.com/nhoaRcZ.jpg',
+    creation_date: '2020-02-06',
+    user_id: 3,
+    featured: false,
+    weight: '400g',
+    city: 'Bread',
     sold: false,
     deleted: false
   }
