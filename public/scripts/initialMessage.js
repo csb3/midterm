4e1b8498e26a6d33240cb589fbaa4bfbbb875c3e
Cass Bredeson <c.s.bredeson@gmail.com>
2021-02-08 21:18:09 +0000

    merge master to dev/sql

--- public/scripts/initialMessage.js
+++ public/scripts/initialMessage.js
@@ -7,6 +7,12 @@ $(() => {
   $messageForm.hide();
 
   $toggleButton.click(event => {
+    $('body, html').animate(
+      {
+        scrollTop: $toggleButton.offset().top
+      }, 800
+    );
+
     $messageForm.slideDown();
   });
 
@@ -15,10 +21,12 @@ $(() => {
     const message = $(this).siblings('#buyerMessage').val();
     $.post('/api/messages/create', message)
       .done(res => {
+        console.log('res',res);
         $messageForm.detach();
         $toggleButton.append('<h2>Message submitted successfully. The seller will be in contact shortly! </h2>');
       })
       .fail(err => {
+        console.log('err', err);
         $messageForm.detach();
         $toggleButton.append('<h2>Message could not be sent. Please refresh the page and try again</h2>');
       });
