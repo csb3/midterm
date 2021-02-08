5c0bc21062da4b868fd87b811159ff9f187b7487
Isaac King <idrkings@gmail.com>
2021-02-08 23:43:36 +0000

    Set up some dummy data for message

--- public/scripts/messaging.js
+++ public/scripts/messaging.js
@@ -1,12 +1,17 @@
 $(() => {
   //fakeConversation
-
-
+  const convos = [
+    conversation1 = {
+      username: 'Dave',
+      item_pic: 'https://i.imgur.com/RTSFzpL.jpg',
+    }
+  ]
 
 
   const $navButton = $('.messages');
 
   $chatWindow = $(`<div id='chatWindow'></div>`)
+  $conversations =
 
 
 
