d06c83e0e57ee125c50f0997ccbc57118987e25f
Cass Bredeson <c.s.bredeson@gmail.com>
2021-02-08 22:12:04 +0000

    remove unused queries, edit listing search criteria

--- routes/listings.js
+++ routes/listings.js
@@ -55,9 +55,12 @@ module.exports = (db) => {
     let queryParams = [];
 
     // base query
-    let queryString = `SELECT *
+    let queryString = `
+    SELECT *
     FROM listings
-    LEFT OUTER JOIN favorites ON listings.id = favorites.listing_id\n`;
+    LEFT OUTER JOIN favorites ON favorites.listing_id = listing.id
+    WHERE sold_date is NULL
+    AND deleted = false`;
 
     // dynamic additions based on search parameters
     if (name) {
@@ -87,7 +90,7 @@ module.exports = (db) => {
     // finish off query
     const limits = 12;
     queryParams.push(limits);
-    queryString += `ORDER BY listings.id LIMIT $${queryParams.length};`;
+    queryString += `ORDER BY creation_date DESC LIMIT $${queryParams.length};`;
 
     // print out the final query that will be run, for debugging only
     printQuery(queryString, queryParams);
