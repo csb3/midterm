af7df0e1f0342e32f5814b263fa661ac3319ef33
Isaac King <idrkings@gmail.com>
2021-02-10 19:16:18 +0000

    Added initial AJAX requests for favourite button on listings

---
+++ public/scripts/favourites.js
@@ -0,0 +1,60 @@
+$(() => {
+
+  const listingID = $('main').attr('id');
+
+  const clearButton = (selector) => {
+    $('.titleBanner').off(selector);
+    $(selector).remove();
+  };
+
+  const makeButton = (style) => {
+    return $button = $(`<button class='favourite><i class="${style} fa-heart"></i></button>`);
+  };
+
+  const isFavourite = (listingID) => {
+
+    clearButton('.favourite');
+    $('.titleBanner').append(makeButton('fas'));
+
+    $('.titleBanner').on('click', '.favourite', listingID, function(event) {
+      // unfavourites the item
+      const listing = event.data;
+      $.post('/unfavourite', listing)
+        .done(data => {
+          if (data) {
+            notFavourite($('main').attr('id'));
+          }
+        })
+        .fail(err => console.error(err));
+    });
+  };
+
+
+  const notFavourite = (listingID) => {
+    clearButton('.favourite');
+    $('.titleBanner').append(makeButton('far'));
+
+    $('.titleBanner').on('click', '.favourite', listingID, function (event) {
+      //favourites the item
+      const listing = event.data;
+      $.post('/makeFavourite', listing)
+        .done(data => {
+          if (data) {
+            isFavourite($('main').attr('id'));
+          }
+        })
+    });
+  }
+
+  $.get('/checkfavourite', listingID)
+    .done(data => {
+      if (data) {
+        isFavourite($('main').attr('id'));
+      } else {
+        notFavourite($('main').attr('id'));
+      }
+    })
+    .fail(err => {
+      console.error(err);
+    })
+});
